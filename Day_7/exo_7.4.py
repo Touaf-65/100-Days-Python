a7a3441dbee81f7f08a819edaeb8293b8b80dab4
Touafic BOUAB <eclairetoilegentlemans@gmail.com>
2024-03-31 07:23:35 -0400

    Exo7.4 Submission

---
+++ Day_7/exo_7.4.py
@@ -0,0 +1,106 @@
+import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+end_of_game = False
+word_list = ["ardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+#variable'lives' to keep track of the number of lives left.
+#By default lives equals to 6.
+lives = 6
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    #Check guessed letter
+    is_guess_used = False
+    for position in range(word_length):
+        letter = chosen_word[position]
+        print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+            is_guess_used = True
+
+    #If guess is not a letter in the chosen_word, then reduce 'lives' by 1.
+    #If lives goes down to 0 then the game should stop and it should print "You lose."
+    if not is_guess_used:
+      lives -= 1
+
+    if lives == 0:
+      end_of_game = True
+      print("You lose.")
+
+
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    print(stages[lives])
